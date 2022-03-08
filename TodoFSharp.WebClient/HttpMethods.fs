35e8432b7a5ee3befd007021bc188243ca719b23
Sten Marius <sten.marius.holmen@gmail.com>
2022-03-08 21:15:11 +0100

    TodoClient use curried functions only

--- TodoFSharp.WebClient/HttpMethods.fs
+++ TodoFSharp.WebClient/HttpMethods.fs
@@ -3,41 +3,41 @@
 open System.Net.Http
 open System.Text
 
-let GET (client: HttpClient) (url: string) =
+let GET<'a> (client: HttpClient) (url: string) =
     async {
         let! response = client.GetAsync(url) |> Async.AwaitTask
-        return! response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content |> Utils.deserialize<'a>
     }
 
-let POST (client: HttpClient) (url: string) =
+let POST<'a> (client: HttpClient) (url: string) =
     async {
         let! response = client.PostAsync(url, null) |> Async.AwaitTask
-        return! response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content |> Utils.deserialize<'a>
     }
 
-let POSTJson (client: HttpClient) (url: string) payload =
+let POSTJson<'a> (client: HttpClient) (url: string) payload =
      async {
-                
         let json = Utils.serialize payload
         let requestBody = new StringContent(json, Encoding.UTF8, "application/json")
         let! response = client.PostAsync(url, requestBody) |> Async.AwaitTask
-        
-        return! response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content |> Utils.deserialize<'a>
      }
      
-let DELETE (client: HttpClient) (url: string) =
+let DELETE<'a> (client: HttpClient) (url: string) =
     async {
         let! response = client.DeleteAsync(url) |> Async.AwaitTask
-        
-        return! response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content |> Utils.deserialize<'a>
     }
 
-let PUTJson (client: HttpClient) (url: string) payload =
+let PUTJson<'a> (client: HttpClient) (url: string) payload =
     async {
         let json = Utils.serialize payload
         let requestBody = new StringContent(json, Encoding.UTF8, "application/json")
-        
         let! response = client.PutAsync(url, requestBody) |> Async.AwaitTask
-        
-        return! response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content |> Utils.deserialize<'a>
     }
--- TodoFSharp.WebClient/TodoClient.fs
+++ TodoFSharp.WebClient/TodoClient.fs
@@ -2,7 +2,6 @@
 
 open System
 open System.Net.Http
-open System.Text
 open TodoFSharp.WebClient.Dto
 open TodoFSharp.WebClient.HttpMethods
 
@@ -12,62 +11,10 @@ let private client =
     client.BaseAddress <- uri
     client
 
-let internal getAllTodoListsRequest () = GET client "/"
-let internal getTodoListRequest name = GET client $"/list/{name}"
-let internal addTodoToListRequest name = POSTJson client $"/list/{name}/todo"
-let internal removeTodoFromListRequest name id = DELETE client $"/list/{name}/todo/{id}"
-let internal updateTodoUrl name = PUTJson client $"/list/{name}/todo"
-let internal completeTodoRequest name id = POST client $"/list/{name}/todo/{id}/complete"
-let internal uncompleteTodoRequest name id = POST client $"/list/{name}/todo/{id}/uncomplete"
-
-
-let getTodoLists () =
-    async {
-        let! response = getAllTodoListsRequest ()
-        
-        return Utils.deserialize<PagedDataDto<TodoListDto>> response 
-    }
-    
-let getTodoList name =
-    async {
-        let! response = getTodoListRequest name
-        
-        let result = Utils.deserialize<TodoListDto> response 
-        
-        return result
-    }
-    
-let addTodoToList (name: string) (todo: TodoDto) =
-    async {
-        let! response = addTodoToListRequest name todo
-        
-        return Utils.deserialize<TodoListDto> response
-    }
-    
-let removeTodoFromList (name: string) (id: Guid) =
-    async {
-        let! response = removeTodoFromListRequest name id
-        
-        return Utils.deserialize<TodoListDto> response
-    }
-    
-let updateTodo (name: string) (todo: TodoDto) =
-    async {
-        let! response = updateTodoUrl name todo
-            
-        return Utils.deserialize<TodoListDto> response
-    }
-    
-let completeTodo (listName: string) (id: string) =
-    async {
-        let! response = completeTodoRequest listName id
-        
-        return Utils.deserialize<TodoListDto> response
-    }
-    
-let uncompleteTodo (listName: string) (id: string) =
-    async {
-        let! response = uncompleteTodoRequest listName id
-        
-        return Utils.deserialize<TodoListDto> response
-    }
+let getTodoLists () = GET<PagedDataDto<TodoListDto>> client "/"
+let getTodoList name = GET<TodoListDto> client $"/list/{name}"
+let addTodoToList name todo = POSTJson<TodoListDto> client $"/list/{name}/todo" todo
+let removeTodoFromList name id = DELETE<TodoListDto> client $"/list/{name}/todo/{id}"
+let updateTodo name todo = PUTJson<TodoListDto> client $"/list/{name}/todo" todo
+let completeTodo name id = POST<TodoListDto> client $"/list/{name}/todo/{id}/complete"
+let uncompleteTodo name id = POST<TodoListDto> client $"/list/{name}/todo/{id}/uncomplete"
